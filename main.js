d9ca5d088b53d7a41fa9a658d9c69c2600a0ba45
yuyu78 <ishiiyuki78@gmail.com>
2021-05-03 04:30:00 +0000

    call back

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-var xhr = new XMLHttpRequest();
+function getData(cb) {
+    var xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
+    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        };
     }
-};
+}
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+function printDataToConsole(data){
+    console.log(data);
+}
 
-xhr.send();
+getData(printDataToConsole);
